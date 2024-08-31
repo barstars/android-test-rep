15547f4282185d953b5ed3795fa70dcc87c873eb
barstars <mtangirkul@gmail.com>
2024-09-01 02:00:38 +0500

    This is android studio commit

---
+++ app/src/main/java/com/barstart/design/MainActivity.java
@@ -0,0 +1,24 @@
+package com.barstart.design;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        hideSystemUI();
+    }
+
+    private void hideSystemUI() {
+        // Включить погружение, что скрывает системные элементы управления
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+}
